cfd9ce2caa122a4b1a1ec48cf10f602df7ee31c3
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-25 19:05:19 -0700

    queries search after 500ms of no change

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,9 +21,17 @@ const Search = () => {
 
       setResults(data.query.search)
     }
-    if (term) {
-      search()
+
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search()
+      }
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId)
     }
+
   }, [term])
 
   const renderedResults = results.map(result => {
