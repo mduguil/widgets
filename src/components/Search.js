aab71dc77ffa3e5fc2aa8b781854ab7ebbd7510f
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-28 11:36:24 -0700

    fixed warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,37 @@ import axios from 'axios'
 
 const Search = () => {
   const [ term, setTerm ] = useState('programming')
+  const [ debouncedTerm, setDebouncedTerm, ] = useState(term)
   const [ results, setResults ] = useState([])
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [term])
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      })
+
+      setResults(data.query.search)
+    }
+    if (debouncedTerm) {
+      search()
+    }
+  }, [debouncedTerm])
 
   useEffect(() => {
     const search = async () => {
@@ -27,13 +57,13 @@ const Search = () => {
         if (term) {
           search()
         }
-      }, 500)
+      }, 1000)
 
       return () => {
         clearTimeout(timeoutId)
       }
     }
-  }, [term])
+  }, [term, results.length])
 
   const renderedResults = results.map(result => {
     return (
